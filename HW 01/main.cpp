a494b2bfde5f78c7ce17b5579afb1ef170e59b6e
LuoZihYuan <fudan28024@gmail.com>
2016-10-10 12:11:41 +0800

    add class myDirtyString

--- HW 01/main.cpp
+++ HW 01/main.cpp
@@ -1,9 +1,6 @@
 #include <iostream>
 #include <string>
-#include "baseString.hpp"
 using namespace std;
 int main(void) {
-	baseString a = "123", b = "abc";
-	cout << a.replace(1, 1, b, 1, -1) << endl;
 	return 0;
 }
