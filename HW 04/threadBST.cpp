c18c7a7ebe73ece28e338ed1166b7eade5cfa7e4
LuoZihYuan <fudan28024@gmail.com>
2016-12-27 01:43:27 +0800

    [HW04] Finish threadBST class

--- HW 04/threadBST.cpp
+++ HW 04/threadBST.cpp
@@ -2,7 +2,9 @@
 /* Constructor */
 threadBST::threadBST() {
 	root = NULL;
-	size = 0;
+	max = NULL;
+	min = NULL;
+	count = 0;
 }
 threadBST::threadBST(const std::vector<int> &v) {
 	for(int num: v) {
@@ -10,21 +12,68 @@ threadBST::threadBST(const std::vector<int> &v) {
 	}
 }
 threadBST::threadBST(const threadBST &tree) {
+	if (tree.count > 0) {
+		std::vector<threadNode*> queue;
+		queue.push_back(tree.root);
+		while(queue.size() > 0) {
+			add(queue[0]->num);
+			if (queue[0]->leftNode != NULL) {
+				queue.push_back(queue[0]->leftNode);
+			}
+			if (queue[0]->rightNode != NULL) {
+				queue.push_back(queue[0]->rightNode);
+			}
+			queue.erase(queue.begin());
+		}
+	}
 }
 /* Destructor */
 threadBST::~threadBST() {
 	delete root;
+}
+/* overload operator << */
+std::ostream& operator<< (std::ostream &os, const threadBST &tree) {
+	if (tree.count > 0) {
+		std::vector<threadNode*> queue;
+		queue.push_back(tree.root);
+		while(queue.size() > 0) {
+			os << queue[0]->num << " ";
+			if (queue[0]->leftNode != NULL) {
+				queue.push_back(queue[0]->leftNode);
+			}
+			if (queue[0]->rightNode != NULL) {
+				queue.push_back(queue[0]->rightNode);
+			}
+			queue.erase(queue.begin());
+		}
+	}
+	return os;
+}
+/* threadBST.size() */
+size_t threadBST::size() const noexcept {
+	return count;
+}
+/* threadBST.clear() */
+void threadBST::clear() noexcept {
+	delete root;
 	root = NULL;
+	max = NULL;
+	min = NULL;
+	count = 0;
+}
+/* threadBST.empty() */
+bool threadBST::empty() const noexcept {
+	return (count == 0) ? true : false;
 }
 /* threadBST.add() */
 void threadBST::add(const int &num) {
 	threadNode *newNode, *currentNode, *previousNode;
 	newNode = new threadNode(num);
-	if (size == 0) {
+	if (count == 0) {
 		root = newNode;
 		min = newNode;
 		max = newNode;
-		size += 1;
+		count += 1;
 	} else {
 		// determine route taken
 		enum Route { left, right, none};
@@ -40,6 +89,7 @@ void threadBST::add(const int &num) {
 				newNode->leftThread = previousNode;
 				lastRoute = right;
 			} else if (newNode->num < currentNode->num) {
+				currentNode->leftSize += 1;
 				previousNode = currentNode;
 				currentNode = currentNode->leftNode;
 				newNode->rightThread = previousNode;
@@ -66,7 +116,47 @@ void threadBST::add(const int &num) {
 			} else {
 				max = newNode;
 			}
-			size += 1;
+			count += 1;
 		}
+	}
+}
+/* threadBST.leftSize() */
+std::vector<int> threadBST::leftSize() const noexcept {
+	std::vector<int> result;
+	if (count > 0) {
+		std::vector<threadNode*> queue;
+		queue.push_back(root);
+		while(queue.size() > 0) {
+			result.push_back(queue[0]->leftSize);
+			if (queue[0]->leftNode != NULL) {
+				queue.push_back(queue[0]->leftNode);
+			}
+			if (queue[0]->rightNode != NULL) {
+				queue.push_back(queue[0]->rightNode);
+			}
+			queue.erase(queue.begin());
+		}
+	}
+	return result;
+}
+/* threadBST.intAtRank() */
+int absolute(int num) {
+	return (num < 0) ? -num : num;
+}
+int threadBST::intAtRank(const int &pos) const {
+	if ((absolute(pos) > count) || (pos == 0)) {
+		throw std::out_of_range("threadBST");
+	} else if (pos > 0) {
+		threadNode *currentNode = max;
+		for (int i = 1; i < pos; i++) {
+			currentNode = currentNode->leftThread;
+		}
+		return currentNode->num;
+	} else {
+		threadNode *currentNode = min;
+		for (int i = -1; i > pos; i--) {
+			currentNode = currentNode->rightThread;
+		}
+		return currentNode->num;
 	}
 }
--- HW 04/threadBST.hpp
+++ HW 04/threadBST.hpp
@@ -15,7 +15,7 @@ private:
 	threadNode *root;
 	threadNode *max;
 	threadNode *min;
-	size_t size;
+	size_t count;
 public:
 	/* Constructor */
 	threadBST();
@@ -23,7 +23,19 @@ public:
 	threadBST(const threadBST&);
 	/* Destructor */
 	~threadBST();
+	/* overload operator << */
+	friend std::ostream& operator<< (std::ostream&, const threadBST&);
+	/* threadBST.size() */
+	size_t size() const noexcept;
+	/* threadBST.clear() */
+	void clear() noexcept;
+	/* threadBST.empty() */
+	bool empty() const noexcept;
 	/* threadBST.add() */
 	void add(const int&);
+	/* threadBST.leftSize() */
+	std::vector<int> leftSize() const noexcept;
+	/* threadBST.intAtRank() */
+	int intAtRank(const int&) const;
 };
 #endif
